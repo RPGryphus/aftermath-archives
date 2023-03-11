10c3c0023e46c671d30209cbb5ebe89f8bb045e8
RPGryphus <126523252+RPGryphus@users.noreply.github.com>
2023-03-11 15:36:43 -0500

    Add files via upload

--- aa-script-adbl.js
+++ aa-script-adbl.js
@@ -1,17 +1,17 @@
 //	ADBLOCKER
-//	Wait for the document to be fully loaded before running the script
-document.addEventListener('DOMContentLoaded', () => {
-	
-//	Select the desired elements
-  const selector = '#main-user-menu a[href^="/u"]';
-  const elements = document.querySelectorAll(selector);
-
-//	Apply the desired styles to matching elements
-  elements.forEach(element => {
-    document.querySelector('#wrap > div[style*="max-height"] iframe:first-of-type').style.display = 'none';
-    document.querySelectorAll('#wrap > div[style*="height"], #wrap > .cp-sidebar.advanced-profile > div[style*="height"]').forEach(el => {
-      el.style.display = 'none';
-      el.style.height = '0px';
+document.addEventListener('DOMContentLoaded', function() {
+  const userMenuLink = document.querySelector('#main-user-menu a[href^="/u"]');
+  if (userMenuLink) {
+    const wrap = document.querySelector('#wrap');
+    const ads = wrap.querySelectorAll('div[style*="max-height"] iframe:first-of-type, div[style*="height"]:first-child, .cp-sidebar.advanced-profile > div[style*="height"]');
+    const css = '.ads, .cp-sidebar.advanced-profile > div[style*="height"] { display: none !important; }';
+    const head = document.head || document.getElementsByTagName('head')[0];
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(css));
+    head.appendChild(style);
+    ads.forEach(ad => {
+      ad.parentNode.removeChild(ad);
     });
-  });
+  }
 });
